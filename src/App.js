0fa61b72afc7cf836cc78fb13dd2c43b1a0c3c17
Elena <ivankina.es@gmail.com>
2021-10-08 11:02:06 +0300

    removed any savings into json file and returned just state

--- src/App.js
+++ src/App.js
@@ -6,58 +6,36 @@ import Footer from "./components/Footer";
 import MainLeft from "./pages/MainLeft";
 import MainRight from "./pages/MainRight";
 
-// import axios from "axios";
-
-// const initialTodos = [
-// 	{
-// 		id: 1,
-// 		title: "do groceries",
-// 		done: true,
-// 		timeSpent: 0,
-// 	},
-// 	{
-// 		id: 2,
-// 		title: "do dishes",
-// 		done: false,
-// 		timeSpent: 150,
-// 	},
-// 	{
-// 		id: 3,
-// 		title: "cleaning",
-// 		done: false,
-// 		timeSpent: 0,
-// 	},
-// ];
-
-const baseURL = "http://localhost:3001/notes";
+const initialTodos = [
+	{
+		id: 1,
+		title: "do groceries",
+		done: true,
+		timeSpent: 0,
+	},
+	{
+		id: 2,
+		title: "do dishes",
+		done: false,
+		timeSpent: 150,
+	},
+	{
+		id: 3,
+		title: "cleaning",
+		done: false,
+		timeSpent: 0,
+	},
+];
 
 const App = () => {
-	const [todosArray, setTodosArray] = useState([]);
-
-	useEffect(() => {
-		axios.get(baseURL).then((response) => {
-			setTodosArray(response.data);
-		});
-	}, [setTodosArray]);
+	const [todosArray, setTodosArray] = useState(initialTodos);
 
 	const toggleTodo = (todo) => {
-		// setTodosArray(
-		// 	todosArray.map((t) => {
-		// 		return t.id === todo.id ? { ...t, done: !t.done } : t;
-		// 	})
-		// );
-
-		const updatedTasks = todosArray.map((t) => {
-			return t.id === todo.id ? { ...t, done: !t.done } : t;
-		});
-		const newItem = {
-			id: todo.id,
-			title: todo.title,
-			done: !todo.done,
-			timeSpent: todo.timeSpent,
-		};
-
-		axios.put(`${baseURL}/${todo.id}`, newItem).then(setTodosArray(updatedTasks));
+		setTodosArray(
+			todosArray.map((t) => {
+				return t.id === todo.id ? { ...t, done: !t.done } : t;
+			})
+		);
 	};
 
 	const addTodo = (todoText) => {
@@ -68,11 +46,8 @@ const App = () => {
 				done: false,
 				timeSpent: 0,
 			};
-			axios.post(baseURL, newTodo).then((resp) => {
-				console.log(resp.data);
-				setTodosArray([resp.data, ...todosArray]);
-			});
-			// setTodosArray([newTodo, ...todosArray]);
+
+			setTodosArray([newTodo, ...todosArray]);
 		}
 	};
 
@@ -80,35 +55,21 @@ const App = () => {
 		const updatedTasks = todosArray.map((t) => {
 			return t.id === todo.id ? { ...t, title: newValue } : t;
 		});
-		const newItem = {
-			id: todo.id,
-			title: newValue,
-			done: todo.done,
-			timeSpent: todo.timeSpent,
-		};
-		if (newValue !== "") {
-			axios.put(`${baseURL}/${todo.id}`, newItem).then(setTodosArray(updatedTasks));
-		}
+		setTodosArray(updatedTasks);
 	};
 
 	const saveTimeIntoTodo = (todo, newTime) => {
 		const updatedTasks = todosArray.map((t) => {
 			return t.id === todo.id ? { ...t, timeSpent: newTime } : t;
 		});
-		const newItem = {
-			id: todo.id,
-			title: todo.title,
-			done: todo.done,
-			timeSpent: newTime,
-		};
 
-		axios.put(`${baseURL}/${todo.id}`, newItem).then(setTodosArray(updatedTasks));
+		setTodosArray(updatedTasks);
 	};
 
 	const deleteTodo = (todo) => {
 		const filteredTasks = [...todosArray.filter((task) => task.id !== todo.id)];
 
-		axios.delete(`${baseURL}/${todo.id}`).then(setTodosArray(filteredTasks));
+		setTodosArray(filteredTasks);
 
 		// setTodosArray([...todosArray.filter((task) => task.id !== todo.id)]);
 	};
